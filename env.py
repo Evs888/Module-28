6648c414ec94fce9b842f408b59a3338d787f54a
Evs888 <emyshalov@gmail.com>
2023-04-13 16:48:57 +0300

    first commit

---
+++ env.py
@@ -0,0 +1,7 @@
+
+valid_email = 'fedorpetrov@trash-mail.com'
+invalid_email = 'fedrpetro@trash-mail.com'
+valid_password = 'Qwerty123'
+invalid_password = 'Qwert1234'
+valid_name = 'Fedor'
+valid_surname = 'Petrov'
